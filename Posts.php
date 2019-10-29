60a05758b69dbe2c8b10fc406929c60c52067bf9
Robert-ikonya-10 <55077333+Robert-ikonya-10@users.noreply.github.com>
2019-10-29 13:22:56 +0300

    News

---
+++ Posts.php
@@ -0,0 +1,144 @@
+<?php
+class Posts extends CI_Controller
+{
+    public function index($offset = 0)
+    {
+        // Pagination Config
+        $config['base_url'] = base_url() . 'posts/index/';
+        $config['total_rows'] = $this->db->count_all('news');
+        $config['per_page'] = 3;
+        $config['uri_segment'] = 3;
+        $config['attributes'] = array('class' => 'pagination-link');
+
+        // Init Pagination
+        $this->pagination->initialize($config);
+
+        $data['title'] = 'Latest News';
+
+        $data['posts'] = $this->post_model->get_posts(false, $config['per_page'], $offset);
+
+        $this->load->view('templates/header');
+        $this->load->view('news/index', $data);
+        // $this->load->view('templates/footer');
+    }
+
+    public function view($slug = null)
+    {
+        $data['post'] = $this->post_model->get_posts($slug);
+        $post_id = $data['post']['id'];
+        // $data['comments'] = $this->comment_model->get_comments($post_id);
+
+        if (empty($data['post'])) {
+            show_404();
+        }
+
+        $data['title'] = $data['post']['title'];
+
+        $this->load->view('templates/header');
+        $this->load->view('news/view', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function create()
+    {
+        // Check login
+        if (!$this->session->userdata('logged_in')) {
+            redirect('users/login');
+        }
+
+        $data['title'] = 'New Post';
+
+        $data['categories'] = $this->post_model->get_categories();
+
+        $this->form_validation->set_rules('title', 'Title', 'required');
+        $this->form_validation->set_rules('body', 'Body', 'required');
+
+        if ($this->form_validation->run() === false) {
+            $this->load->view('admin/header');
+            $this->load->view('admin/create_post', $data);
+            $this->load->view('admin/footer');
+        } else {
+            // Upload Image
+            $config['upload_path'] = './assets/images/posts';
+            $config['allowed_types'] = 'gif|jpg|png|tif';
+            $config['max_size'] = '2048';
+            $config['max_width'] = '2000';
+            $config['max_height'] = '2000';
+
+            $this->load->library('upload', $config);
+
+            if (!$this->upload->do_upload()) {
+                $errors = array('error' => $this->upload->display_errors());
+                $post_image = 'noimage.jpg';
+            } else {
+                $data = array('upload_data' => $this->upload->data());
+                $post_image = $_FILES['userfile']['name'];
+            }
+
+            $this->post_model->create_post($post_image);
+
+            // Set message
+            $this->session->set_flashdata('post_created', 'Your post has been created');
+
+            redirect('posts');
+        }
+    }
+
+    public function delete($id)
+    {
+        // Check login
+        if (!$this->session->userdata('logged_in')) {
+            redirect('users/login');
+        }
+
+        $this->post_model->delete_post($id);
+
+        // Set message
+        $this->session->set_flashdata('post_deleted', 'Your post has been deleted');
+
+        redirect('posts');
+    }
+
+    public function edit($slug)
+    {
+        // Check login
+        if (!$this->session->userdata('logged_in')) {
+            redirect('users/login');
+        }
+
+        $data['post'] = $this->post_model->get_posts($slug);
+
+        // Check user
+        if ($this->session->userdata('user_id') != $this->post_model->get_posts($slug)['user_id']) {
+            redirect('posts');
+
+        }
+
+        $data['categories'] = $this->post_model->get_categories();
+
+        if (empty($data['post'])) {
+            show_404();
+        }
+
+        $data['title'] = 'Edit Post';
+
+        $this->load->view('templates/header');
+        $this->load->view('news/edit', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function update()
+    {
+        // Check login
+        if (!$this->session->userdata('logged_in')) {
+            redirect('users/login');
+        }
+
+        $this->post_model->update_post();
+
+        // Set message
+        $this->session->set_flashdata('post_updated', 'Your post has been updated');
+
+        redirect('posts');
+    }
+}
